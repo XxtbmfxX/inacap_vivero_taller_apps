f23ec7e2dc2357a5fcc72609670cd3f60a84777a
RFRomeroS <raimundo.romero@inacapmail.cl>
2024-06-10 16:48:16 -0400

    Update CardSemillas.jsx
    card_semillas

--- components/cards_de_items/CardSemillas.jsx
+++ components/cards_de_items/CardSemillas.jsx
@@ -15,23 +15,100 @@ const CardSemillas = ({
   procedencia = "Máfil",
 }) => {
   return (
-    <Card>
+    
+<Card.Title>
+  Semillas
+</Card.Title>
+<Card.Divider />
+<View
+  style={{
+    position: "relative",
+    alignItems: "left",
+    backgroundColor:
+      "lightgray",
+    width: 350,
+    height: 200,
+    borderTopLeftRadius: 15,
+    borderTopRightRadius: 15,
+    borderBottomLeftRadius: 15,
+    borderBottomRightRadius: 15
+  }}
+>
+  <View
+    style={{
+      flex: 10,
+      flexDirection: "row",
+      gap: 20,
+      margin: 15,
+      alignItems: 'baseline',
 
+    }}
+  >
+    <Text
+      style={{
+        fontSize: 20,
+        fontWeight: "bold"
+      }}
+    >
+      Despacho
+    </Text>
+    <Text
+      style={{
+        fontSize: 15
+      }}
+    >
+      Cantidad #
+    </Text>
+  </View>
 
+  <View
+    style={{
+      flex: 30,
+      flexDirection: "row",
+      gap: 20,
+      margin: 10
+    }}
+  >
+    <Text>
+      FechaColecta
+    </Text>
+    <Text>
+      FechaCosecha
+    </Text>
+  </View>
 
-      <View style="" >
-        <Text>{despacho}</Text>
-        <Text>{cantidad}</Text>
-        <Text>{fechaColecta}</Text>
-        <Text>{fechaCosecha}</Text>
-      </View>
+  <View
+    style={{
+      flex: 1,
+      flexDirection: "row",
+      gap: 50,
+      margin: 10
+    }}
+  >
+    <Text>
+      Peso 100 Semillas
+    </Text>
+    <Text>
+      Porcentaje
+      Germinación#
+    </Text>
+  </View>
 
-      <Text>{peso100Semillas}</Text>
-      <Text>{porcentajeGerminación}</Text>
-      <Text>{códigoBolsa}</Text>
-      <Text>{condición}</Text>
-      <Text>{procedencia}</Text>
-    </Card>
+  <View
+    style={{
+      flex: 1,
+      flexDirection: "row",
+      gap: 30,
+      margin: 40,
+      alignItems: "bottom"
+    }}
+  >
+    <Text>Cod.Bolsa</Text>
+    <Text>Condición</Text>
+    <Text>Procedencia</Text>
+  </View>
+</View>
+
   );
 };
 

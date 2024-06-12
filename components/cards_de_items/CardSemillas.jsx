cca27746e9d13d453eadec366e2bda70166b8d18
Toro <aatn1321@gmail.com>
2024-06-12 13:28:30 -0400

    minnor fix

--- components/cards_de_items/CardSemillas.jsx
+++ components/cards_de_items/CardSemillas.jsx
@@ -15,100 +15,89 @@ const CardSemillas = ({
   procedencia = "Máfil",
 }) => {
   return (
+    <>
+      <Card.Title>Semillas</Card.Title>
+      <Card.Divider />
+      <View
+        style={{
+          position: "relative",
+          alignItems: "left",
+          backgroundColor: "lightgray",
+          width: 350,
+          height: 200,
+          borderTopLeftRadius: 15,
+          borderTopRightRadius: 15,
+          borderBottomLeftRadius: 15,
+          borderBottomRightRadius: 15,
+        }}
+      >
+        <View
+          style={{
+            flex: 10,
+            flexDirection: "row",
+            gap: 20,
+            margin: 15,
+            alignItems: "baseline",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 20,
+              fontWeight: "bold",
+            }}
+          >
+            Despacho
+          </Text>
+          <Text
+            style={{
+              fontSize: 15,
+            }}
+          >
+            Cantidad #
+          </Text>
+        </View>
+
+        <View
+          style={{
+            flex: 30,
+            flexDirection: "row",
+            gap: 20,
+            margin: 10,
+          }}
+        >
+          <Text>FechaColecta</Text>
+          <Text>FechaCosecha</Text>
+        </View>
+
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            gap: 50,
+            margin: 10,
+          }}
+        >
+          <Text>Peso 100 Semillas</Text>
+          <Text>Porcentaje Germinación#</Text>
+        </View>
+
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            gap: 30,
+            margin: 40,
+            alignItems: "bottom",
+          }}
+        >
+          <Text>Cod.Bolsa</Text>
+          <Text>Condición</Text>
+          <Text>Procedencia</Text>
+        </View>
+      </View>
+    </>
+
     
-<Card.Title>
-  Semillas
-</Card.Title>
-<Card.Divider />
-<View
-  style={{
-    position: "relative",
-    alignItems: "left",
-    backgroundColor:
-      "lightgray",
-    width: 350,
-    height: 200,
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
-    borderBottomLeftRadius: 15,
-    borderBottomRightRadius: 15
-  }}
->
-  <View
-    style={{
-      flex: 10,
-      flexDirection: "row",
-      gap: 20,
-      margin: 15,
-      alignItems: 'baseline',
-
-    }}
-  >
-    <Text
-      style={{
-        fontSize: 20,
-        fontWeight: "bold"
-      }}
-    >
-      Despacho
-    </Text>
-    <Text
-      style={{
-        fontSize: 15
-      }}
-    >
-      Cantidad #
-    </Text>
-  </View>
-
-  <View
-    style={{
-      flex: 30,
-      flexDirection: "row",
-      gap: 20,
-      margin: 10
-    }}
-  >
-    <Text>
-      FechaColecta
-    </Text>
-    <Text>
-      FechaCosecha
-    </Text>
-  </View>
-
-  <View
-    style={{
-      flex: 1,
-      flexDirection: "row",
-      gap: 50,
-      margin: 10
-    }}
-  >
-    <Text>
-      Peso 100 Semillas
-    </Text>
-    <Text>
-      Porcentaje
-      Germinación#
-    </Text>
-  </View>
-
-  <View
-    style={{
-      flex: 1,
-      flexDirection: "row",
-      gap: 30,
-      margin: 40,
-      alignItems: "bottom"
-    }}
-  >
-    <Text>Cod.Bolsa</Text>
-    <Text>Condición</Text>
-    <Text>Procedencia</Text>
-  </View>
-</View>
-
   );
 };
 

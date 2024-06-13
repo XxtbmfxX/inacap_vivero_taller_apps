f67ff012facc937dc0fe8ca51833823b4a2d0548
RFRomeroS <raimundo.romero@inacapmail.cl>
2024-06-12 20:50:20 -0400

    Update CardPlantas.jsx
    plantas

--- components/cards_de_items/CardPlantas.jsx
+++ components/cards_de_items/CardPlantas.jsx
@@ -8,5 +8,80 @@ const CardPlantas = () => {
     </View>
   )
 }
+<>
+  <Card.Title>
+    Plantas
+  </Card.Title>
+  <Card.Divider />
+  <View
+    style={{
+      position: "relative",
+      alignItems: "left",
+      backgroundColor:
+        "lightgray",
+      width: 300,
+      height: 150,
+      borderTopLeftRadius: 15,
+      borderTopRightRadius: 15,
+      borderBottomLeftRadius: 15,
+      borderBottomRightRadius: 15
+    }}
+  >
+    <View
+      style={{
+        flex: 10,
+        flexDirection:
+          "row",
+        gap: 100,
+        margin: 15,
+        alignItems:
+          "baseline"
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: "bold"
+        }}
+      >
+        Especie
+      </Text>
+      
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: "bold"
+        }}
+      >
+        Sector
+      </Text>
+    </View>
+
+    <View
+      style={{
+        flex: 100,
+        flexDirection:
+          "row",
+        gap: 100,
+        margin: 15
+      }}
+    >
+      <Text>
+        Año Despacho
+      </Text>
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: "bold"
+        }}
+      >
+        #
+      </Text>
+    </View>
+
+    
+   
+  </View>
+</>
 
 export default CardPlantas

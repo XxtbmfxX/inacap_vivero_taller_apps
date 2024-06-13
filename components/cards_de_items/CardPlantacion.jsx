e48775afd9ffba133af65b3702d0d788fd542705
RFRomeroS <raimundo.romero@inacapmail.cl>
2024-06-12 21:20:04 -0400

    Update CardPlantacion.jsx

--- components/cards_de_items/CardPlantacion.jsx
+++ components/cards_de_items/CardPlantacion.jsx
@@ -9,4 +9,70 @@ const CardPlantacion = () => {
   )
 }
 
+<>
+  <Card.Title>
+    Plantación
+  </Card.Title>
+  <Card.Divider />
+  <View
+    style={{
+      position: "relative",
+      alignItems: "left",
+      backgroundColor:
+        "lightgray",
+      width: 300,
+      height: 150,
+      borderTopLeftRadius: 15,
+      borderTopRightRadius: 15,
+      borderBottomLeftRadius: 15,
+      borderBottomRightRadius: 15
+    }}
+  >
+    <View
+      style={{
+        flex: 40,
+        flexDirection:
+          "row",
+        gap: 80,
+        margin: 15,
+        alignItems:
+          "baseline"
+      }}
+    >
+      <Text>
+        FechaInicio
+      </Text>
+      
+      <Text>
+        FechaTermino
+      </Text>
+    </View>
+
+    <View
+      style={{
+        flex: 10,
+        flexDirection:
+          "row",
+        gap: 40,
+        margin: 15
+      }}
+    >
+      <Text>
+      Especie
+      </Text>
+      <Text>
+      Especie 2
+      </Text>
+      <Text>
+     N° Cosecha #
+      </Text>
+
+    </View>
+
+    
+   
+  </View>
+</>
+
+
 export default CardPlantacion

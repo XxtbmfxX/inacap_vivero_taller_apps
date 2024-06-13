eb5ea3713e0c806fdfe54525b341695b99d4cbe0
RFRomeroS <raimundo.romero@inacapmail.cl>
2024-06-12 20:36:56 -0400

    Update CardQuimicos.jsx
    card quimicos

--- components/cards_de_items/CardQuimicos.jsx
+++ components/cards_de_items/CardQuimicos.jsx
@@ -9,4 +9,121 @@ const CardQuimicos = () => {
   )
 }
 
+<>
+  <Card.Title>
+    Químicos
+  </Card.Title>
+  <Card.Divider />
+  <View
+    style={{
+      position: "relative",
+      alignItems: "left",
+      backgroundColor:
+        "lightgray",
+      width: 350,
+      height: 200,
+      borderTopLeftRadius: 15,
+      borderTopRightRadius: 15,
+      borderBottomLeftRadius: 15,
+      borderBottomRightRadius: 15
+    }}
+  >
+    <View
+      style={{
+        flex: 10,
+        flexDirection:
+          "row",
+        gap: 20,
+        margin: 15,
+        alignItems:
+          "baseline"
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: "bold"
+        }}
+      >
+        Nombre
+      </Text>
+    </View>
+
+    <View
+      style={{
+        flex: 20,
+        flexDirection:
+          "row",
+        gap: 20,
+        margin: 15
+      }}
+    >
+      <Text>
+        FechaIngreso
+      </Text>
+    </View>
+
+    <View
+      style={{
+        flex: 1,
+        flexDirection:
+          "row",
+        gap: 50,
+        margin: 10
+      }}
+    >
+      <Text></Text>
+      <Text></Text>
+      <Text></Text>
+      <Text>
+        Cantidad
+        #(stock)/#(stock
+        minimo)
+      </Text>
+    </View>
+
+    <View
+      style={{
+        flex: 100,
+        flexDirection:
+          "row",
+        gap: 30,
+        margin: 15
+      }}
+    >
+      <Text>Contenido</Text>
+    </View>
+
+    <View
+      style={{
+        flex: 10000,
+        flexDirection:
+          "row",
+        gap: 30,
+        margin: 15
+      }}
+    >
+      <Text>
+        Necesidades lt/kg
+      </Text>
+    </View>
+
+    <View
+      style={{
+        flex: 1800,
+        flexDirection:
+          "row",
+        gap: 150,
+        margin: 15
+      }}
+    >
+      <Text>Categoría</Text>
+      <Text>
+        Presentación
+      </Text>
+    </View>
+  </View>
+</>
+
+
 export default CardQuimicos

6037927b488e82177199fe385b92d92911dd0521
RFRomeroS <raimundo.romero@inacapmail.cl>
2024-06-13 23:39:10 -0400

    Update CardMaterial.jsx

--- components/cards_de_items/CardMaterial.jsx
+++ components/cards_de_items/CardMaterial.jsx
@@ -9,4 +9,77 @@ const CardMaterial = () => {
   )
 }
 
+ <>
+  <Card.Title>
+    Materiales
+  </Card.Title>
+  <Card.Divider />
+  <View
+    style={{
+      position: "relative",
+      alignItems: "left",
+      backgroundColor:
+        "lightgray",
+      width: 300,
+      height: 130,
+      borderTopLeftRadius: 15,
+      borderTopRightRadius: 15,
+      borderBottomLeftRadius: 15,
+      borderBottomRightRadius: 15
+    }}
+  >
+    <View
+      style={{
+        flex: 1,
+        flexDirection:
+          "row",
+        gap: 80,
+        margin: 25,
+        alignItems:
+          "baseline"
+      }}
+    >
+      <Text style = {{fontSize: 17}}>
+        Nombre
+      </Text>
+
+    </View>
+
+    <View
+      style={{
+        flex: 10,
+        flexDirection:
+          "row",
+        gap: 40,
+        margin: 15
+      }}
+    >
+      <Text style>
+      Unidad de medida
+      </Text>
+
+    </View>
+    <View
+      style={{
+        flex: 10,
+        flexDirection:
+          "row",
+        gap: 47,
+        margin: 10
+      }}
+    >
+    <Text></Text>
+        <Text></Text>
+            <Text></Text>
+                <Text></Text>
+      <Text>
+      Cantidad #
+      </Text>
+
+    </View>
+    
+   
+  </View>
+</>
+
 export default CardMaterial
